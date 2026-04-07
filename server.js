bd665a504ee9b801733180b87e3c9f6bad6dbe15
mrhamzaa9 <hamzathor470@gmail.com>
2026-04-07 14:11:27 +0500

    db connect

---
+++ server.js
@@ -0,0 +1,33 @@
+const express = require("express");
+const mongoose = require("./db/connection");
+const cors = require("cors");
+const bodyParser = require("body-parser");
+const cookieParser = require("cookie-parser");
+require("dotenv").config();
+
+
+
+
+const app = express();
+
+// middlewares
+app.use(express.json());
+app.use(
+  cors({
+    origin: true,
+    credentials: true,
+  })
+);
+app.use(cookieParser());
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(bodyParser.json());
+// routes
+
+
+
+
+
+const PORT = process.env.API_PORT ;
+app.listen(PORT, () => {
+  console.log(`API  running on ${PORT}`);
+});
